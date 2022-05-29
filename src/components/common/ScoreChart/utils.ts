000fa4a98a571e0d7f4331bceb9d2ec16d6f59a7
Min Jiwon <peace.cheejecake@gmail.com>
2022-05-29 14:35:22 +0900

    refactor: Remove unused util file

--- src/components/common/ScoreChart/utils.ts
+++
@@ -1,9 +0,0 @@
-// export const getComputedFontSize = (fontSize: number | string) => {
-//   if (typeof fontSize === 'number') return fontSize;
-
-//   const l = fontSize.length;
-
-//   if (fontSize.substring(l - 3) === 'rem')
-// }
-
-export {};
